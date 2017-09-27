61d1af9b196917305739273e7dcc303654c48391
winifredand <31584601+winifredand@users.noreply.github.com>
2017-09-27 10:27:36 +0800

    Update trackfit.cu

--- trackfit.cu
+++ trackfit.cu
@@ -58,7 +58,7 @@ extern "C" void CircleFitG(double X[HIT], double Y[HIT], double Z[HIT], double Z
 	dim3 dimBlock2(HIT, 1);// HIT=20,此句表示每个block有20个线程；
 	dim3 dimGrid2(1, 1); //表示1*1个block；
 
-	 /////////时间函数///////////
+	/////////时间函数///////////
 	cudaEvent_t start, stop;
 	float time_kernel;
 	checkCudaErrors(cudaEventRecord(start, 0));//开始计时；
@@ -75,13 +75,14 @@ extern "C" void CircleFitG(double X[HIT], double Y[HIT], double Z[HIT], double Z
 	checkCudaErrors(cudaEventDestroy(start));//destory the event
 	checkCudaErrors(cudaEventDestroy(stop));
 
-	printf("kernel:\t\t%.2f\n", time_kernel);//输出内核函数运行时间；
+	printf("Fit的核函数kernel运行时间:\t\t%.2f\n", time_kernel);//输出内核函数运行时间；
 
 	//////////////将结果从显存设备段写入内存主机端/////////////
 	checkCudaErrors(cudaMemcpy(result, d_result, size * 8, cudaMemcpyDeviceToHost));
 
 	/////////////打印结果/////////
-	printf("%f, %f, %f, %f, %f, %f, %f, %f,\n",result[0],result[1],result[2],result[3],result[4],result[5],result[6],result[7]);
+	printf("Newton迭代法运行结果1: \n");
+	printf("%f, %f, %f, %f, %f, %f, %f, %f,\n", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7]);
 
 	/////////////释放显存////////
 Error:
@@ -99,13 +100,6 @@ Error:
 
 extern "C" void CircleFitGAllD(double X[TRK*HIT], double Y[TRK*HIT], double Z[TRK*HIT], double Zerr[TRK*HIT], double Mx[TRK], double My[TRK], double M0[TRK], double result[TRK * 8])
 {
-	//   printf(" Now in Cuda :  Mx =  %g  :  My =  %g  \n", Mx[0],My[0] );
-	/* for(int j=0; j<100; j++){
-	for(int i=0; i<HIT; i++){
-	printf("%d  Zerr[%i] =  %g  :  Z[%i] =  %g  \n", j ,i, Zerr[i+HIT*j],i ,Z[i+HIT*j]  );
-	}
-	}
-	*/
 	double *d_X;
 	double *d_Y;
 	double *d_Z;
@@ -115,75 +109,91 @@ extern "C" void CircleFitGAllD(double X[TRK*HIT], double Y[TRK*HIT], double Z[TR
 	double *d_M0;
 	double *d_result;
 
+	cudaError_t cudaStatus;
+
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
+
 	/*   result[0]=1;
 	result[1]=1;
 	result[2]=1;*/
 
 	size_t size = sizeof(double);
 	//allocate memory for arrays on device 
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_X, size*HIT*TRK));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_Y, size*HIT*TRK));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_Z, size*HIT*TRK));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_Zerr, size*HIT*TRK));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_Mx, size*TRK));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_My, size*TRK));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_M0, size*TRK));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_result, size * 8 * TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_X, size*HIT*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_Y, size*HIT*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_Z, size*HIT*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_Zerr, size*HIT*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_Mx, size*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_My, size*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_M0, size*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_result, size * 8 * TRK));
 
+	checkCudaErrors(cudaMemcpy(d_X, X, size*HIT*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Y, Y, size*HIT*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Z, Z, size*HIT*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Zerr, Zerr, size*HIT*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Mx, Mx, size*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_My, My, size*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_M0, M0, size*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_result, result, size * 8 * TRK, cudaMemcpyHostToDevice));
 
-
-	CUDA_SAFE_CALL(cudaMemcpy(d_X, X, size*HIT*TRK, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_Y, Y, size*HIT*TRK, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_Z, Z, size*HIT*TRK, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_Zerr, Zerr, size*HIT*TRK, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_Mx, Mx, size*TRK, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_My, My, size*TRK, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_M0, M0, size*TRK, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_result, result, size * 8 * TRK, cudaMemcpyHostToDevice));
-
-
-	int threads = HIT;
-	int tracks = TRK;
+	int threads = HIT;//HIT = 30;
+	int tracks = TRK;// TRK = 2000;
 	dim3 dimBlock2(threads, 1);
 	dim3 dimGrid2(tracks, 1);
 
-	// FitAllD<<< dimGrid2, dimBlock2 >>> (d_X, d_Y,d_Z, d_Zerr,d_Mx,d_My,d_M0, d_result);
+	cudaEvent_t start, stop;
+	float time_kernel;
+	checkCudaErrors(cudaEventRecord(start, 0));
 
+	FitAllD << < dimGrid2, dimBlock2 >> > (d_X, d_Y, d_Z, d_Zerr, d_Mx, d_My, d_M0, d_result);
+
+	checkCudaErrors(cudaEventRecord(stop, 0));
+	checkCudaErrors(cudaEventSynchronize(start));
+	checkCudaErrors(cudaEventSynchronize(stop));
+
+	checkCudaErrors(cudaEventElapsedTime(&time_kernel, start, stop));
+	checkCudaErrors(cudaEventDestroy(start));
+	checkCudaErrors(cudaEventDestroy(stop));
+
+	printf("FitALLD的核函数kernel运行时间:\t\t%.2f\n", time_kernel);
+
+	checkCudaErrors(cudaMemcpy(result, d_result, size * 8 * TRK, cudaMemcpyDeviceToHost));
+
+	printf("Newton迭代法运行结果2: \n");
+	printf("%f, %f, %f, %f, %f, %f, %f, %f,\n", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7]);
 	// cudaThreadSynchronize();
-	printf(" Now calling the device  \n");
-	CUDA_SAFE_CALL(cudaMemcpy(result, d_result, size * 8 * TRK, cudaMemcpyDeviceToHost));
 
-	/*  for(int j=0; j<tracks; j++){
-	printf("%d   ",j);
-	for(int i=0; i<8; i++){
-	printf("  %f ", result[i+8*j]);
+	for (int j = 0; j<tracks; j++)
+	{
+		printf("%d   ", j);
+		for (int i = 0; i<8; i++)
+		{
+			printf("  %f ", result[i + 8 * j]);
+		}
+		printf(" \n");
 	}
-	printf(" \n");
-	}
-	*/
-	//   printf(" Now cleaning device memory  \n");
-	CUDA_SAFE_CALL(cudaFree(d_X));
-	CUDA_SAFE_CALL(cudaFree(d_Y));
-	CUDA_SAFE_CALL(cudaFree(d_Z));
-	CUDA_SAFE_CALL(cudaFree(d_Zerr));
-	CUDA_SAFE_CALL(cudaFree(d_Mx));
-	CUDA_SAFE_CALL(cudaFree(d_My));
-	CUDA_SAFE_CALL(cudaFree(d_M0));
-	CUDA_SAFE_CALL(cudaFree(d_result));
-	//   printf(" Finish cleaning device memory  \n");
+
+Error:
+	checkCudaErrors(cudaFree(d_X));
+	checkCudaErrors(cudaFree(d_Y));
+	checkCudaErrors(cudaFree(d_Z));
+	checkCudaErrors(cudaFree(d_Zerr));
+	checkCudaErrors(cudaFree(d_Mx));
+	checkCudaErrors(cudaFree(d_My));
+	checkCudaErrors(cudaFree(d_M0));
+	checkCudaErrors(cudaFree(d_result));
 }
 
 
 
 extern "C" void CircleFitGAllF(float X[TRK*HIT], float Y[TRK*HIT], float Z[TRK*HIT], float Zerr[TRK*HIT], float Mx[TRK], float My[TRK], float M0[TRK], float result[8 * TRK])
 {
-	//   printf(" Now in Cuda :  Mx =  %g  :  My =  %g  \n", Mx[0],My[0] );
-	/* for(int j=0; j<100; j++){
-	for(int i=0; i<HIT; i++){
-	printf("%d  Zerr[%i] =  %g  :  Z[%i] =  %g  \n", j ,i, Zerr[i+HIT*j],i ,Z[i+HIT*j]  );
-	}
-	}
-	*/
 	float *d_X;
 	float *d_Y;
 	float *d_Z;
@@ -197,71 +207,82 @@ extern "C" void CircleFitGAllF(float X[TRK*HIT], float Y[TRK*HIT], float Z[TRK*H
 	result[1]=1;
 	result[2]=1;*/
 
+	cudaError_t cudaStatus;
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
+
 	size_t size = sizeof(float);
-	//allocate memory for arrays on device 
-	cudaMalloc((void **)&d_X, size*HIT*TRK);
-	cudaMalloc((void **)&d_Y, size*HIT*TRK);
-	cudaMalloc((void **)&d_Z, size*HIT*TRK);
-	cudaMalloc((void **)&d_Zerr, size*HIT*TRK);
-	cudaMalloc((void **)&d_Mx, size*TRK);
-	cudaMalloc((void **)&d_My, size*TRK);
-	cudaMalloc((void **)&d_M0, size*TRK);
-	cudaMalloc((void **)&d_result, size * 8 * TRK);
+	checkCudaErrors(cudaMalloc((void **)&d_X, size*HIT*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_Y, size*HIT*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_Z, size*HIT*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_Zerr, size*HIT*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_Mx, size*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_My, size*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_M0, size*TRK));
+	checkCudaErrors(cudaMalloc((void **)&d_result, size * 8 * TRK));
 
-
-
-	cudaMemcpy(d_X, X, size*HIT*TRK, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_Y, Y, size*HIT*TRK, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_Z, Z, size*HIT*TRK, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_Zerr, Zerr, size*HIT*TRK, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_Mx, Mx, size*TRK, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_My, My, size*TRK, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_M0, M0, size*TRK, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_result, result, size * 8 * TRK, cudaMemcpyHostToDevice);
-
+	checkCudaErrors(cudaMemcpy(d_X, X, size*HIT*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Y, Y, size*HIT*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Z, Z, size*HIT*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Zerr, Zerr, size*HIT*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Mx, Mx, size*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_My, My, size*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_M0, M0, size*TRK, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_result, result, size * 8 * TRK, cudaMemcpyHostToDevice));
 
 	int threads = HIT;
 	int tracks = TRK;
 	dim3 dimBlock2(threads, 1);
 	dim3 dimGrid2(tracks, 1);
 
-	// FitAllF<<< dimGrid2, dimBlock2 >>> (d_X, d_Y,d_Z, d_Zerr,d_Mx,d_My,d_M0, d_result);
+	cudaEvent_t start, stop;
+	float time_kernel;
+	checkCudaErrors(cudaEventRecord(start, 0));
 
-	//   cudaThreadSynchronize();
+	FitAllF << < dimGrid2, dimBlock2 >> > (d_X, d_Y, d_Z, d_Zerr, d_Mx, d_My, d_M0, d_result);
 
-	cudaMemcpy(result, d_result, size * 8 * TRK, cudaMemcpyDeviceToHost);
+	checkCudaErrors(cudaEventRecord(stop, 0));
+	checkCudaErrors(cudaEventSynchronize(start));
+	checkCudaErrors(cudaEventSynchronize(stop));
 
-	/*  for(int j=0; j<tracks; j++){
-	printf("%d   ",j);
-	for(int i=0; i<8; i++){
-	printf("  %f ", result[i+8*j]);
+	checkCudaErrors(cudaEventElapsedTime(&time_kernel, start, stop));
+	checkCudaErrors(cudaEventDestroy(start));
+	checkCudaErrors(cudaEventDestroy(stop));
+
+	printf("FitALLF的核函数kernel运行时间:\t\t%.2f\n", time_kernel);
+
+	//cudaThreadSynchronize();
+
+	checkCudaErrors(cudaMemcpy(result, d_result, size * 8 * TRK, cudaMemcpyDeviceToHost));
+
+	printf("Newton迭代法运行结果3: \n");
+	//	printf("%f, %f, %f, %f, %f, %f, %f, %f,\n",result[0],result[1],result[2],result[3],result[4],result[5],result[        6],result[7]);
+	for (int j = 0; j<tracks; j++)
+	{
+		printf("%d", j);
+		for (int i = 0; i<8; i++){
+			printf("  %f ", result[i + 8 * j]);
+		}
+		printf(" \n");
 	}
-	printf(" \n");
-	}
-	*/
-	// printf(" Now cleaning device memory  \n");
-	CUDA_SAFE_CALL(cudaFree(d_X));
-	CUDA_SAFE_CALL(cudaFree(d_Y));
-	CUDA_SAFE_CALL(cudaFree(d_Z));
-	CUDA_SAFE_CALL(cudaFree(d_Zerr));
-	CUDA_SAFE_CALL(cudaFree(d_Mx));
-	CUDA_SAFE_CALL(cudaFree(d_My));
-	CUDA_SAFE_CALL(cudaFree(d_M0));
-	CUDA_SAFE_CALL(cudaFree(d_result));
-	//   printf(" Finish cleaning device memory  \n");
+
+Error:
+	checkCudaErrors(cudaFree(d_X));
+	checkCudaErrors(cudaFree(d_Y));
+	checkCudaErrors(cudaFree(d_Z));
+	checkCudaErrors(cudaFree(d_Zerr));
+	checkCudaErrors(cudaFree(d_Mx));
+	checkCudaErrors(cudaFree(d_My));
+	checkCudaErrors(cudaFree(d_M0));
+	checkCudaErrors(cudaFree(d_result));
 }
-
-
-
 
 extern "C" void CircleFitGF(float X[HIT], float Y[HIT], float Z[HIT], float Zerr[HIT], float *Mx, float *My, float *M0, float *result)
 {
-	//   printf(" Now in Cuda :  Mx =  %g  :  My =  %g  \n", Mx[0],My[0] );
-
-	/*  for(int i=0; i<50; i++){
-	printf(" Zerr[%i] =  %g  :  Z[%i] =  %g  \n", i, Zerr[i],i ,Z[i]  );
-	}
-	*/
 	float *d_X;
 	float *d_Y;
 	float *d_Z;
@@ -271,50 +292,63 @@ extern "C" void CircleFitGF(float X[HIT], float Y[HIT], float Z[HIT], float Zerr
 	float *d_M0;
 	float *d_result;
 
-	/*   result[0]=1;
-	result[1]=1;
-	result[2]=1;*/
+	cudaError_t cudaStatus;
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess)
+	{
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
 
 	size_t size = sizeof(float);
-	//allocate memory for arrays on device 
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_X, size*HIT));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_Y, size*HIT));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_Z, size*HIT));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_Zerr, size*HIT));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_Mx, size));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_My, size));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_M0, size));
-	CUDA_SAFE_CALL(cudaMalloc((void **)&d_result, size * 8));
+	checkCudaErrors(cudaMalloc((void **)&d_X, size*HIT));
+	checkCudaErrors(cudaMalloc((void **)&d_Y, size*HIT));
+	checkCudaErrors(cudaMalloc((void **)&d_Z, size*HIT));
+	checkCudaErrors(cudaMalloc((void **)&d_Zerr, size*HIT));
+	checkCudaErrors(cudaMalloc((void **)&d_Mx, size));
+	checkCudaErrors(cudaMalloc((void **)&d_My, size));
+	checkCudaErrors(cudaMalloc((void **)&d_M0, size));
+	checkCudaErrors(cudaMalloc((void **)&d_result, size * 8));
 
-
-
-	CUDA_SAFE_CALL(cudaMemcpy(d_X, X, size*HIT, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_Y, Y, size*HIT, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_Z, Z, size*HIT, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_Zerr, Zerr, size*HIT, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_Mx, Mx, size, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_My, My, size, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_M0, M0, size, cudaMemcpyHostToDevice));
-	CUDA_SAFE_CALL(cudaMemcpy(d_result, result, size * 8, cudaMemcpyHostToDevice));
-
+	checkCudaErrors(cudaMemcpy(d_X, X, size*HIT, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Y, Y, size*HIT, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Z, Z, size*HIT, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Zerr, Zerr, size*HIT, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_Mx, Mx, size, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_My, My, size, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_M0, M0, size, cudaMemcpyHostToDevice));
+	checkCudaErrors(cudaMemcpy(d_result, result, size * 8, cudaMemcpyHostToDevice));
 
 	dim3 dimBlock2(HIT, 1);
 	dim3 dimGrid2(1, 1);
+	cudaEvent_t start, stop;
+	float time_kernel;
+	checkCudaErrors(cudaEventRecord(start, 0));
 
-	FitF << < dimGrid2, dimBlock2 >> > (d_X, d_Y, d_Z, d_Zerr, d_Mx, d_My, d_M0, d_result);
+	FitF <<< dimGrid2, dimBlock2 >>> (d_X, d_Y, d_Z, d_Zerr, d_Mx, d_My, d_M0, d_result);
 
+	checkCudaErrors(cudaEventRecord(stop, 0));
+	checkCudaErrors(cudaEventSynchronize(start));
+	checkCudaErrors(cudaEventSynchronize(stop));
 
-	CUDA_SAFE_CALL(cudaMemcpy(result, d_result, size * 8, cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaEventElapsedTime(&time_kernel, start, stop));
+	checkCudaErrors(cudaEventDestroy(start));
+	checkCudaErrors(cudaEventDestroy(stop));
 
-	//  printf(" %f      %f      %f  %   f      %f     %f     %f       %f \n", result[0], result[1],result[2], result[3] ,result[4],result[5] ,result[6],result[7]);
+	printf("FitF的核函数kernel运行时间:\t\t%.2f\n", time_kernel);
 
-	CUDA_SAFE_CALL(cudaFree(d_X));
-	CUDA_SAFE_CALL(cudaFree(d_Y));
-	CUDA_SAFE_CALL(cudaFree(d_Z));
-	CUDA_SAFE_CALL(cudaFree(d_Zerr));
-	CUDA_SAFE_CALL(cudaFree(d_Mx));
-	CUDA_SAFE_CALL(cudaFree(d_My));
-	CUDA_SAFE_CALL(cudaFree(d_M0));
-	CUDA_SAFE_CALL(cudaFree(d_result));
+	checkCudaErrors(cudaMemcpy(result, d_result, size * 8, cudaMemcpyDeviceToHost));
 
+	printf("Newton迭代法运行结果4: \n");
+	printf("%f, %f, %f, %f, %f, %f, %f, %f,\n", result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7]);
+
+Error:
+	checkCudaErrors(cudaFree(d_X));
+	checkCudaErrors(cudaFree(d_Y));
+	checkCudaErrors(cudaFree(d_Z));
+	checkCudaErrors(cudaFree(d_Zerr));
+	checkCudaErrors(cudaFree(d_Mx));
+	checkCudaErrors(cudaFree(d_My));
+	checkCudaErrors(cudaFree(d_M0));
+	checkCudaErrors(cudaFree(d_result));
 }
